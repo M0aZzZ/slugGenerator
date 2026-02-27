b742b422381f1125e508ca7b1881533cb5182a86
m0az <moazkm2@gmail.com>
2026-02-27 08:41:58 -0800

    update slug code

--- src/index.ts
+++ src/index.ts
@@ -24,10 +24,3 @@ function Remove_unwanted_characters(url: string): string {
     url = (url.endsWith('-')) ? url.slice(0, -1) : url;
     return url;
 }
-// url = url.trim();
-//     url = url.toLowerCase();
-//     url = url.replace(/[\s-_,.]+/g, '-');//remove the whitespaces and sperators within url
-//     url = url.replace(/[\#@!]+/g, '');//remove the special characters from url
-//     //To ensure there are no Unwanted characters at the beginning and end of the url
-//     url = (url.startsWith('-')) ? url.slice(1) : url;
-//     url = (url.endsWith('-')) ? url.slice(0, -1) : url;
