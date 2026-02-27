a4d1bba5544f6dcef8b1fa9acdb878421e91fe1f
m0az <moazkm2@gmail.com>
2026-02-26 22:16:22 -0800

    add the slugify function && edits

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-export function slugify(text: string): string {
-    return "test if this function exported correctly";
+export const slugify = (url: string): string => {
+    url = url.trim();
+    url = url.toLowerCase();
+    url = url.replace(/[\s-_,.]+/g, '-');//remove the whitespaces and sperators within url
+    url = url.replace(/[\#@!]+/g, '');//remove the special characters from url
+    //To ensure there are no Unwanted characters at the beginning and end of the url
+    url = (url.startsWith('-')) ? url.slice(1) : url;
+    url = (url.endsWith('-')) ? url.slice(0, -1) : url;
+    return url;
 }
