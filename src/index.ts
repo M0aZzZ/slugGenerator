2eb60d7e0d0bbdd237bf9a8f32140e65601283db
m0az <moazkm2@gmail.com>
2026-03-01 19:00:34 -0800

    update index file

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,14 @@
 export const slugify = (url: string): string => {
-    let clean_url: string = url;
-    clean_url = Convert_to_lowercase(clean_url);
-    clean_url = Remove_spaces(clean_url);
-    clean_url = Remove_separators(clean_url);
-    clean_url = Remove_special_characters(clean_url);
-    clean_url = Remove_unwanted_characters(clean_url);
-    return clean_url;
+    let cleanUrl: string = url;
+    cleanUrl = cleanUrl
+        .toLowerCase()
+        .trim()
+        .replace(/[\s-_,.]+/g, '-')
+        .replace(/[\#@!]+/g, '')
+    cleanUrl = trimDashes(cleanUrl);
+    return cleanUrl;
 }
-function Convert_to_lowercase(url: string): string {
-    return url.toLowerCase();
-}
-function Remove_spaces(url: string): string {
-    return url.trim();
-}
-function Remove_separators(url: string): string {
-    return url.replace(/[\s-_,.]+/g, '-');
-}
-function Remove_special_characters(url: string): string {
-    return url.replace(/[\#@!]+/g, '');
-}
-function Remove_unwanted_characters(url: string): string {
+const trimDashes = (url: string): string => {
     url = (url.startsWith('-')) ? url.slice(1) : url;
     url = (url.endsWith('-')) ? url.slice(0, -1) : url;
     return url;
