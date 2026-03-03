50f8d033516f3e31fc787c55624e3dcaca523fa7
m0az <moazkm2@gmail.com>
2026-03-02 22:22:46 -0800

    simple fixes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 export const slugify = (url: string): string => {
-    let cleanUrl: string = url;
-    if (typeof cleanUrl !== "string") {
-        throw new TypeError("slugify: input must be a string");
+    if (typeof url !== "string") {
+    throw new TypeError("slugify: input must be a string");
     }
+    let cleanUrl: string = url;
     cleanUrl = cleanUrl
         .toLowerCase()
         .trim()
